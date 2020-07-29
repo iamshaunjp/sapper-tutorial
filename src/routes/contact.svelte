a73e18856da0c7f8bff2aea47fbe254ea45ae9d6
Shaun <shaun@thenetninja.co.uk>
2020-07-29 15:37:40 +0100

    lesson-2

---
+++ src/routes/contact.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Contact Job Ninja</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fugit perferendis aliquam harum aperiam dolorum sapiente natus voluptatum suscipit corrupti, consequuntur provident. Maxime culpa doloribus optio suscipit qui earum veritatis, quidem vel ex sunt impedit libero officia quod fugiat dolore autem, voluptatum expedita tenetur eius? Veritatis harum autem quaerat atque.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fugit perferendis aliquam harum aperiam dolorum sapiente natus voluptatum suscipit corrupti, consequuntur provident. Maxime culpa doloribus optio suscipit qui earum veritatis, quidem vel ex sunt impedit libero officia quod fugiat dolore autem, voluptatum expedita tenetur eius? Veritatis harum autem quaerat atque.</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,44 @@
+<style>
+	h1, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+	h1 {
+		font-size: 2em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+	img {
+		width: 200px;
+		margin: 0 auto;
+    display: block;
+	}
+	p {
+		margin: 1em auto;
+  }
+  a {
+    margin-top: 40px;
+  }
+  div {
+    margin-top: 60px;
+    text-align: center;
+  }
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Job Ninja</title>
+</svelte:head>
+
+<div>
+  <img src='no-text-logo.png' alt='ninja logo'>
+  <h1>Jobs for Ninjas!</h1>
+  <p>Find your next assignment right here.</p>
+  <a href="jobs" class="btn">View Jobs</a>
+</div>
