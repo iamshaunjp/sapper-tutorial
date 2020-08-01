fc3c0002a930f09c6b46d417e5c01600d462f11a
Shaun <9838872+iamshaunjp@users.noreply.github.com>
2020-08-01 12:29:14 +0100

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -8,37 +8,37 @@
 
 <style>
 	nav {
-    background: #fff3ef;
+	    background: #fff3ef;
 	}
-  nav div {
-    max-width: 960px;
-    padding: 0 10px;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
-  }
-  h1 {
-    margin-top: 10px;
-    font-size: 22px;
-    font-weight: bold;
-  }
+	nav div {
+		max-width: 960px;
+		padding: 0 10px;
+		margin: 0 auto;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		align-items: center;
+	}
+	h1 {
+		margin-top: 10px;
+		font-size: 22px;
+		font-weight: bold;
+	}
 	ul {
 		padding: 0;
-    margin: 0;
-    text-align: right;
+   		margin: 0;
+    		text-align: right;
 	}
-  li {
-    display: inline-block;
-  }
+	li {
+		display: inline-block;
+	}
 	a {
 		text-decoration: none;
 		padding: 1em 0.5em;
 		display: block;
 	}
-  a.current {
-    border-bottom: 3px solid #ff3e00;
-  }
+	a.current {
+		border-bottom: 3px solid #ff3e00;
+	}
 </style>
 
 <nav>
